6af6c2f4fd52908375e2e7a6aebdcec397a59d28
gordon_lew <seednaruto123@gmail.com>
2024-10-26 19:27:20 +0800

    Update for nextjs deploy

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
   images: {
     domains: ['cdn.dummyjson.com'],
     // loader: "custom",
     // loaderFile: "./src/loader/image.js",
   },
+  output: 'export'
 };
 
 export default nextConfig;
